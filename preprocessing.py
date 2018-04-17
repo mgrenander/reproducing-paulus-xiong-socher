a4acb9d4495baf7b23a4e10948b57ca1c4f03917
mgrenander <matt.grenander@gmail.com>
2018-04-17 18:33:13 -0400

    debugging preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ from torchtext import data
 import pandas as pd
 import sys
 import os
+from tqdm import tqdm
 
 base_path = "data/finished_files"
 def convert_to_tsv(dataset):
@@ -9,7 +10,7 @@ def convert_to_tsv(dataset):
     ref_path = os.path.join(base_path, "reference", dataset)
 
     df = pd.DataFrame(columns=['article', 'reference'], dtype=str)
-    for i in range(len(os.listdir(art_path))):
+    for i in tqdm(range(len(os.listdir(art_path)))):
         article_name = str(i) + "_" + dataset + "_art.txt"
         ref_name = str(i) + "_" + dataset + "_ref.txt"
         article = open(os.path.join(art_path, article_name), encoding='utf-8')
@@ -18,8 +19,15 @@ def convert_to_tsv(dataset):
         row = pd.Series(index=['article', 'reference'], data=[article.read(), reference.read()], dtype=str)
         df.append(row, ignore_index=True)
 
+    print(df)
+
     df.to_csv(path_or_buf=os.path.join("data", dataset + ".tsv"), sep='\t', columns=['article', 'reference'],
               header=False, index=False, chunksize=1000)
 
-dataset = sys.argv[1]
-convert_to_tsv(dataset)
+if __name__ == "__main__":
+    datasets = ["test"]
+    for dataset in datasets:
+        if not os.path.exists(dataset + ".tsv"):
+            print("Creating TSV for " + dataset)
+            convert_to_tsv(dataset)
+
