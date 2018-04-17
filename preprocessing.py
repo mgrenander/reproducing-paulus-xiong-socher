05aeac2063228dc05c376aa8c684f9b0dfd163d8
mgrenander <matt.grenander@gmail.com>
2018-04-17 18:03:07 -0400

    first commit

---
+++ preprocessing.py
@@ -0,0 +1,23 @@
+from torchtext import data
+import pandas as pd
+import sys
+import os
+
+def convert_to_tsv(dataset):
+    art_path = os.path.join("article", dataset)
+    ref_path = os.path.join("reference", dataset)
+
+    df = pd.DataFrame(columns=['article', 'reference'], dtype=str)
+    for i in range(len(os.listdir(art_path))):
+        article_name = str(i) + "_" + dataset + "_art.txt"
+        ref_name = str(i) + "_" + dataset + "_ref.txt"
+        article = open(os.path.join(art_path, article_name), encoding='utf-8')
+        reference = open(os.path.join(ref_path, ref_name), encoding='utf-8')
+
+        row = pd.Series(index=['article', 'reference'], data=[article.read(), reference.read()], dtype=str)
+
+        print(row)
+        break
+
+dataset = sys.argv[1]
+convert_to_tsv(dataset)
